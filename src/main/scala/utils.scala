1dd1f3717b53b72f911778c2ac4ac887fe5c1c51
James Leo <james@jamesleo.co.uk>
2019-06-05 16:53:24 +0800

    Added factory object

--- src/main/scala/utils.scala
+++ src/main/scala/utils.scala
@@ -1,5 +1,7 @@
+import Element.elem
 
 abstract class Element {
+
   def contents: Array[String] // each String in the Array represents a line
 
   def height: Int = contents.length  // parameterless method
@@ -8,9 +10,9 @@ abstract class Element {
   def width: Int = if (height == 0) 0 else contents(0).length // parameterless method
   // val width = if (height == 0) 0 else contents(0).length // implemented as a field
 
-  def above(that: Element): Element = new ArrayElement(this.contents ++ that.contents)
+  def above(that: Element): Element = elem(this.contents ++ that.contents)
 
-  def besides(that: Element) : Element = new ArrayElement(
+  def besides(that: Element) : Element = elem(
     for (
       (line1, line2) <- this.contents zip that.contents
     ) yield line1 + line2
@@ -19,17 +21,28 @@ abstract class Element {
   override def toString = contents mkString "\n"
 }
 
-class ArrayElement(const: Array[String]) extends Element {
-  val contents: Array[String] = const
-}
 
-class LineElement(s: String) extends Element {
-  val contents = Array(s)
-  override def width = s.length
-  override def height = 1
-}
 
-class UniformElement (ch: Char, override val width:Int, override val height: Int) extends Element {
-  private val line = ch.toString * width
-  def contents = Array.fill(height)(line)
+object Element {
+
+  private class ArrayElement(val contents: Array[String]) extends Element {
+  }
+
+  private class LineElement(s: String) extends Element {
+    val contents = Array(s)
+    override def width = s.length
+    override def height = 1
+  }
+
+  private class UniformElement (ch: Char, override val width:Int, override val height: Int) extends Element {
+    private val line = ch.toString * width
+    def contents = Array.fill(height)(line)
+  }
+
+
+  def elem(contents: Array[String]): Element = new ArrayElement(contents)
+
+  def elem(chr: Char, width: Int, height: Int): Element = new UniformElement(chr, width, height)
+
+  def elem(line: String): Element = new LineElement(line)
 }
